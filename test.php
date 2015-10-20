29ec022b5fe5e55e4efd4d71352f9b37fe70ee95
aa791845283 <kevin.liu@yunzhihui.com>
2015-10-20 17:14:26 +0800

    test

--- test.php
+++ test.php
@@ -10,24 +10,24 @@ if(isset($_GET['a'])){
     $act = $_GET['a'];
     switch($act){
         case 'one':
-            rangeFun(0,15);
+            rangeFun(0,100);
             break;
         case 'two':
-            rangeFun(0,20);
+            rangeFun(0,200);
             break;
         case 'three':
-            rangeFun(0,30);
+            rangeFun(0,300);
             break;
     }
 }else{
-    rangeFun(0,15);
+    rangeFun(0,150);
 }
 
 function rangeFun($a,$b){
     $num = rand($a,$b);
-    if($num < 5){
+    if($num < 20){
         header("HTTP/1.1 404 Not Found");
-    }else if($num>5 && $num<10){
+    }else if($num>20 && $num<150){
         $s_rand = rand(0,10);
         if($s_rand > 7){
             sleep(1);
