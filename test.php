10811fa05fc8fdaa2c0f96b8123f3d7ad4addf5a
aa791845283 <kevin.liu@yunzhihui.com>
2015-10-20 11:54:26 +0800

    处理了test

--- test.php
+++ test.php
@@ -6,16 +6,34 @@
  * Time: 上午12:36
  */
 //header("Status: 404 Not Found");
-
-$num = rand(0,15);
-if($num < 5){
-    header("HTTP/1.1 404 Not Found");
-}else if($num>5 && $num<10){
-    $s_rand = rand(0,10);
-    if($s_rand > 7){
-        sleep(1);
+if(isset($_GET['a'])){
+    $act = $_GET['a'];
+    switch($act){
+        case 'one':
+            rangeFun(0,15);
+            break;
+        case 'two':
+            rangeFun(0,20);
+            break;
+        case 'three':
+            rangeFun(0,30);
+            break;
     }
-    echo '111';
 }else{
-    echo '3333';
+    rangeFun(0,15);
+}
+
+function rangeFun($a,$b){
+    $num = rand($a,$b);
+    if($num < 5){
+        header("HTTP/1.1 404 Not Found");
+    }else if($num>5 && $num<10){
+        $s_rand = rand(0,10);
+        if($s_rand > 7){
+            sleep(1);
+        }
+        echo '111';
+    }else{
+        echo '3333';
+    }
 }
